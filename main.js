99ca6977397b3cc1adc1b151768550ee4a6a526b
superpatateultime <mlkjhgfdsqnbvcxw44@gmail.com>
2021-01-30 23:08:27 +0100

    absolument rien

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 ///début du code principale de elybot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('ptdrtacruquoi');
