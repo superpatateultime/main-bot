4bd93ef5c071b37529533f64437cb3d7385d236a
superpatateultime <mlkjhgfdsqnbvcxw44@gmail.com>
2021-02-01 13:43:31 +0100

    le main.js

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 ///début du code principale de elybot
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  client.user.setActivity('alys et kilebot qui se font une soirée netflx and chibre euh chill', { type: 'WATCHING' })
+  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+  .catch(console.error);
+  var embed = new Discord.RichEmbed()
+  .setAuthor("INFORMATION!", client.user.avatarURL)
+  .setDescription("LE BOT A DEMARRE. UTILISATION DU CHAT OK!")
+  .setColor(16768000)
+  .setFooter(client.user.name, client.user.avatarURL)
+  var guildList = client.guilds.array();
+  for(i=0; i<guildList.length; i++){
+    if(guildList[i].channels.exists("name", 'psikhì-chat')){
+      guildList[i].channels.find("name", 'psikhì-chat').send(embed)
+    }
   }
 });
 
-client.login('ptdrtacruquoi');
+client.on("message", message => {
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command=="dis"){
+    message.delete();
+    const trimmedArray = message.content.split("p!dis")[1];
+    message.channel.send(trimmedArray)
+    
+  }
+});
+
+client.login('');
